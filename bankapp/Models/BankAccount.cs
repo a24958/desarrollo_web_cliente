4506e2a82d6834d97d3399ab196685fb15058d6e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2023-10-23 18:45:57 +0200

    Clase 23/10/2023

--- bankapp/Models/BankAccount.cs
+++ bankapp/Models/BankAccount.cs
@@ -3,30 +3,69 @@ namespace Models;
 public class BankAccount {
     public string? Number {get;}
     public string? Owner {get; set;}
-    public decimal Balance {get;}
+    public decimal Balance {
+        get{
+            decimal  balance = 0;
+            foreach (Transaction item in transactions){
+                balance += item.Amount;
+            }
+            return balance;
+        }
+    }
 
     private static int accountNumber_seed = 1;
 
+    private List<Transaction> transactions = new List<Transaction>();
+
     public BankAccount(string owner){
         this.Owner = owner;
-        this.Balance = 0;
         this.Number = accountNumber_seed.ToString();
         accountNumber_seed++;
     }
 
     public BankAccount(string owner, decimal balance){
         this.Owner = owner;
-        this.Balance =  balance;
+        MakeDeposit(balance, DateTime.Now, "First deposit");
         this.Number = accountNumber_seed.ToString();
         accountNumber_seed++;
     }
 
-    public void MakeDeposit(decimal ammount, DateTime date, string note) {
-        
+    public void MakeDeposit(decimal amount, DateTime date, string note) {
+        if (amount <= 0){
+            throw new ArgumentOutOfRangeException(nameof(amount), "No puede añadirse un depósito negativo");
+        }
+
+        if (date > DateTime.Now){
+            throw new InvalidDataException("La fecha no puede ser posterior a la actual");
+        }
+       
+        var deposit = new Transaction(amount, date, note);
+        transactions.Add(deposit);
     }
 
-    public void Makewithdrawal(decimal ammount, DateTime date, string note) {
+    public void Makewithdrawal(decimal amount, DateTime date, string note) {
+        if (amount <= 0){
+            throw new ArgumentOutOfRangeException(nameof(amount), "No puede realizar una retirada negativa");
+        }
 
+        if((Balance - amount) < 0){
+            throw new InvalidOperationException("No puedes retirar dinero, porque no dispone de él");
+        }
+
+        var withdrawal = new Transaction(-amount, date, note);
+        transactions.Add(withdrawal);
+    }
+
+    public string GetAccountHistory(){
+        var history = new System.Text.StringBuilder();
+        decimal balance = 0;
+        history.AppendLine("Date\t\tAmount\tBalance\tNote");
+        foreach (var item in transactions){
+            balance += item.Amount;
+            history.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Note}");
+        }
+
+        return history.ToString();
     }
 
     public override string ToString(){
--- bankapp/Program.cs
+++ bankapp/Program.cs
@@ -3,9 +3,40 @@
 Console.WriteLine("Hello, World!");
 
 BankAccount account = new BankAccount("Alex", 100);
+var today = DateTime.Now;
+
+account.MakeDeposit(5000, today, "Colacao");
+account.MakeDeposit(2000, today, "Churros");
+
+// try{
+//     account.MakeDeposit(-2000, today, "Churros");
+// } catch (ArgumentOutOfRangeException e){
+//     Console.WriteLine("ArgumentOutOfRangeException: " + e.ToString());
+// } catch (Exception e){
+//     Console.WriteLine("Exception: " + e.ToString());
+// }
+
+try{
+    account.Makewithdrawal(200, today, "Bus");
+} catch (ArgumentOutOfRangeException e){
+    Console.WriteLine("ArgumentOutOfRangeException: " + e.ToString());
+} catch (InvalidOperationException e){
+    Console.WriteLine("InvalidOperationException: ", e.ToString());
+}
+
+// try{
+//     account.Makewithdrawal(-200, today, "Bus");
+// } catch (ArgumentOutOfRangeException e){
+//     Console.WriteLine("ArgumentOutOfRangeException: " + e.ToString());
+// } catch (InvalidOperationException e){
+//     Console.WriteLine("InvalidOperationException: ", e.ToString());
+// }
+
+var balance = account.Balance;
 var stringifAccount =  account.ToString();
 Console.WriteLine(stringifAccount);
 
-BankAccount account2 = new BankAccount("Aaron");
+var history = account.GetAccountHistory();
+Console.WriteLine(history);
 
 Console.WriteLine("Bye, World!");
